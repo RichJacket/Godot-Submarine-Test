bd6bc2d8e53f460d1fc30b68f046c172755883cb
RichJacket <lenaoxtonetracer@gmail.com>
2026-04-08 11:46:25 +0300

    #1 - 6DOF movement; - Rotation with quaternions; - Smoothing with interpolation

---
+++ player.gd
@@ -0,0 +1,64 @@
+extends CharacterBody3D
+
+@export var max_speed = 3
+@export var rotation_speed = 3
+
+var current_speed = 0
+var angular_velocity : Quaternion
+var linear_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	#Rotate player
+	var pitch = 0
+	var yaw = 0
+	var roll = 0
+	
+	if Input.is_action_pressed("pitchUp"):
+		pitch += rotation_speed
+	if Input.is_action_pressed("pitchDown"):
+		pitch -= rotation_speed
+	if Input.is_action_pressed("yawLeft"):
+		yaw += rotation_speed
+	if Input.is_action_pressed("yawRight"):
+		yaw -= rotation_speed
+	if Input.is_action_pressed("rollLeft"):
+		roll -= rotation_speed
+	if Input.is_action_pressed("rollRight"):
+		roll += rotation_speed
+	
+	var rotation_vector = Vector3(deg_to_rad(roll), deg_to_rad(yaw), deg_to_rad(pitch)) * delta
+	var relative_velocity = Quaternion.from_euler(rotation_vector)
+	angular_velocity = (angular_velocity * relative_velocity).slerp(Quaternion.IDENTITY, 0.1)
+	
+	var current_rotation = Quaternion(transform.basis)
+	var pre_final_rotation = current_rotation * angular_velocity
+	var euler_angles = pre_final_rotation.get_euler()
+	var final_rotation = pre_final_rotation.slerp(Quaternion.from_euler(Vector3(0, euler_angles.y, 0)), 0.005)
+	transform.basis = Basis(final_rotation)
+	
+	transform = transform.orthonormalized()
+	
+	pitch = 0
+	yaw = 0
+	
+	#Move player
+	var linear_direction = Vector3.ZERO
+	if Input.is_action_pressed("accelerateForward"):
+		linear_direction.x += 1
+	if Input.is_action_pressed("accelerateBackward"):
+		linear_direction.x -= 1
+	if Input.is_action_pressed("accelerateLeft"):
+		linear_direction.z -= 1
+	if Input.is_action_pressed("accelerateRight"):
+		linear_direction.z += 1
+	if Input.is_action_pressed("accelerateUp"):
+		linear_direction.y += 1
+	if Input.is_action_pressed("accelerateDown"):
+		linear_direction.y -= 1
+	
+	if linear_direction != Vector3.ZERO:
+		linear_direction = linear_direction.normalized()
+	linear_velocity = final_rotation * linear_direction
+	
+	velocity = velocity.move_toward(linear_velocity * max_speed, 0.1)
+	move_and_slide()
