0efbcc70a6569b76eb2d21180ee843901e5bd03a
RichJacket <lenaoxtonetracer@gmail.com>
2026-05-11 12:42:11 +0300

    #4 - Fixed controls; - Added obstacles

--- controller.gd
+++ controller.gd
@@ -1,24 +1,29 @@
 extends AIController3D
 
 @onready var raycast_sensor := $RayCastSensor3D
+@onready var goal = $/root/Main/Goal
 
 var rotate_action : Vector3
 var move_action : Vector3
+var is_success := false
 
-func _physics_process(_delta):
-	n_steps += 1
-	# A time-out for the player
-	if n_steps > reset_after:
-		_player.game_over(-5)
+func get_info() -> Dictionary:
+	if done: 
+		return {"is_success": is_success}
+	return {}
 
+#Get observation space data (raycast data; player position, velocity, rotation)
 func get_obs() -> Dictionary:
-	# Get the raycast data & player position, velocity & rotation
 	var obs: Array[float] = []
-	obs.append_array(raycast_sensor.get_observation())
+	#obs.append_array(raycast_sensor.get_observation())
 	var player_position = _player.global_position
 	var player_velocity = _player.velocity
 	var player_angle = _player.transform.basis.get_euler()
+	var goal_position = _player.to_local(goal.global_position)
 	obs.append_array([
+		goal_position.x,
+		goal_position.z,
+		goal_position.y,
 		player_position.x,
 		player_position.z,
 		player_position.y,
@@ -29,32 +34,33 @@ func get_obs() -> Dictionary:
 		player_angle.z,
 		player_angle.y,
 	])
-
 	return {"obs":obs}
 
 func get_reward() -> float:	
 	return reward
 
-## Resets the AIController (e.g. when game is over)
+#Reset the AIController (e.g. when game is over)
 func reset():
 	super.reset()
 
+#Get action space (movement actions)
 func get_action_space() -> Dictionary:
 	return {
 		"rotate_action" : {
-			"size": 3,
+			"size": 2,
 			"action_type": "continuous"
 		},
 		"move_action" : {
-			"size": 3,
+			"size": 1,
 			"action_type": "continuous"
 		},
 		}
-	
+
+#Complete chosen action (apply movement)
 func set_action(action) -> void:	
-	rotate_action.x = clamp(action["move_action"][0], -1.0, 1.0)
-	rotate_action.z = clamp(action["move_action"][1], -1.0, 1.0)
-	rotate_action.y = clamp(action["move_action"][2], -1.0, 1.0)
+	rotate_action.x = clamp(action["rotate_action"][0], -1.0, 1.0)
+	rotate_action.z = clamp(action["rotate_action"][1], -1.0, 1.0)
+	#rotate_action.y = clamp(action["rotate_action"][2], -1.0, 1.0)
 	move_action.x = clamp(action["move_action"][0], -1.0, 1.0)
-	move_action.z = clamp(action["move_action"][1], -1.0, 1.0)
-	move_action.y = clamp(action["move_action"][2], -1.0, 1.0)
+	#move_action.z = clamp(action["move_action"][1], -1.0, 1.0)
+	#move_action.y = clamp(action["move_action"][1], -1.0, 1.0) #Change index to 2 for 6DOF
--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends CharacterBody3D
 @onready var goal = $/root/Main/Goal
 @onready var initial_transform := transform
 @onready var best_distance_to_goal = global_position.distance_to(goal.global_position)
+@onready var current_distance_to_goal = global_position.distance_to(goal.global_position)
+#@onready var previous_distance_to_goal = global_position.distance_to(goal.global_position)
 
 @export var max_speed = 3
 @export var rotation_speed = 3
@@ -11,21 +13,35 @@ extends CharacterBody3D
 var current_speed = 0
 var angular_velocity : Quaternion
 var linear_velocity = Vector3.ZERO
-var current_distance_to_goal : float
+var reward = 0
+var n_steps = 0
 
+#Initialize the AI controller
 func _ready():
 	ai_controller.init(self)
 
-func game_over(reward: float = 0):
-	ai_controller.reward += reward
+#Reset environment on game over (time-out, obstacle crash, goal reached)
+func game_over(ep_reward: float = 0.0):
+	ai_controller.reward += ep_reward
 	ai_controller.done = true
 	ai_controller.reset()
 	transform = initial_transform
+	reward = 0
+	n_steps = 0
+	#current_distance_to_goal = global_position.distance_to(goal.global_position)
+	#previous_distance_to_goal = current_distance_to_goal
 
 func _physics_process(_delta):
+	if !(ai_controller.heuristic) == "human":
+		n_steps += 1
+		# A time-out for the player
+		if n_steps > ai_controller.reset_after:
+			game_over(reward - 25)
+	
 	if ai_controller.needs_reset:
 		ai_controller.reset()
 		return
+	
 	#Rotate player
 	var pitch = 0
 	var yaw = 0
@@ -39,16 +55,16 @@ func _physics_process(_delta):
 			yaw += rotation_speed
 		if Input.is_action_pressed("yawRight"):
 			yaw -= rotation_speed
-		if Input.is_action_pressed("rollLeft"):
-			roll -= rotation_speed
-		if Input.is_action_pressed("rollRight"):
-			roll += rotation_speed
+		#if Input.is_action_pressed("rollLeft"):
+			#roll -= rotation_speed
+		#if Input.is_action_pressed("rollRight"):
+			#roll += rotation_speed
 	else:
 		pitch = ai_controller.rotate_action.x * rotation_speed
 		yaw = ai_controller.rotate_action.z * rotation_speed
-		roll = ai_controller.rotate_action.y * rotation_speed
+		#roll = ai_controller.rotate_action.y * rotation_speed
 	if ai_controller.rotate_action != Vector3.ZERO:
-		ai_controller.reward -= 0.1
+		reward -= 0.1
 	
 	var rotation_vector = Vector3(deg_to_rad(roll), deg_to_rad(yaw), deg_to_rad(pitch)) * _delta
 	var relative_velocity = Quaternion.from_euler(rotation_vector)
@@ -72,33 +88,38 @@ func _physics_process(_delta):
 			linear_direction.x += 1
 		if Input.is_action_pressed("accelerateBackward"):
 			linear_direction.x -= 1
-		if Input.is_action_pressed("accelerateLeft"):
-			linear_direction.z -= 1
-		if Input.is_action_pressed("accelerateRight"):
-			linear_direction.z += 1
-		if Input.is_action_pressed("accelerateUp"):
-			linear_direction.y += 1
-		if Input.is_action_pressed("accelerateDown"):
-			linear_direction.y -= 1
+		#if Input.is_action_pressed("accelerateLeft"):
+			#linear_direction.z -= 1
+		#if Input.is_action_pressed("accelerateRight"):
+			#linear_direction.z += 1
+		#if Input.is_action_pressed("accelerateUp"):
+			#linear_direction.y += 1
+		#if Input.is_action_pressed("accelerateDown"):
+			#linear_direction.y -= 1
 	else:
 		linear_direction.x = ai_controller.move_action.x
-		linear_direction.z = ai_controller.move_action.z
-		linear_direction.y = ai_controller.move_action.y
+		#linear_direction.z = ai_controller.move_action.z
+		#linear_direction.y = ai_controller.move_action.y
 	
 	if linear_direction != Vector3.ZERO:
 		linear_direction = linear_direction.normalized()
-		ai_controller.reward -= 0.1
+		reward -= 0.1
 	linear_velocity = final_rotation * linear_direction
 	
 	velocity = velocity.move_toward(linear_velocity * max_speed, 0.1)
 	move_and_slide()
 	if move_and_slide() == true:
-		game_over(-20)
+		game_over(reward - 50)
+	
+	#current_distance_to_goal = global_position.distance_to(goal.global_position)
+	#ai_controller.reward += (previous_distance_to_goal - current_distance_to_goal) / 1
+	#previous_distance_to_goal = current_distance_to_goal
 	
 	current_distance_to_goal = global_position.distance_to(goal.global_position)
 	if current_distance_to_goal < best_distance_to_goal:
-		ai_controller.reward += 1
+		reward += 0.5
 	best_distance_to_goal = current_distance_to_goal
 
 func _on_goal_body_entered(_body: Node3D) -> void:
+	ai_controller.is_success = true
 	game_over(1000)
